1ee959eee15bc3f1d0143087d0b0908296c2845d
Kunshan Wang <wks1986@gmail.com>
2010-09-14 14:06:41 +0800

    Added command line parameters for dumpboard.py script.

--- dumpboard.py
+++ dumpboard.py
@@ -1,21 +1,54 @@
+#!/usr/bin/env python
+# coding: utf_8
+
 import byr3
 import pickle
 import sys
+import getopt
+
+USAGE="""dumpboard.py: Dump a board in the BYR forum.
+Usage: python dumpboard.py [args]...
+
+Parameters:
+    -b, --board-name:   Name of the board to dump.  Default: Linux
+    -l, --limit-pages:  Limit the total number of pages to dump.  Each page has
+                        at most 60 threads.  Default: None
+    -o, --output:       The output file name.  Default: threads.pickle
+    -h, --help:         Show this help message.
+"""
+
+try:
+    optlist, args = getopt.getopt(sys.argv[1:], "b:l:o:h", [
+        "board-name", "limit-pages", "output", "help"])
+except getopt.GetoptError, err:
+    print str(err)
+    print USAGE
+    sys.exit(2)
+
+board_name = "Linux"
+limit_pages = None
+output_file_name = "threads.pickle"
+
+for o,a in optlist:
+    if o in ['-b', '--board-name']:
+        board_name = a
+    elif o in ["-l", "--limit-pages"]:
+        limit_pages = int(a)
+    elif o in ["-o", "--output"]:
+        output_file_name = a
+    elif o in ["-h", "--help"]:
+        print USAGE
+        sys.exit()
 
 byr = byr3.Byr()
 
-board = byr.board("Linux")
-
-try:
-    pickle_file = sys.argv[1]
-except IndexError:
-    pickle_file = "threads.pickle"
+board = byr.board(board_name)
 
 print "loading threads..."
-threads = board.threads()
+threads = board.threads(limit_pages)
 print "threads loaded!  %d threads to go." % len(threads)
 
-with open(pickle_file,"w") as f:
+with open(output_file_name,"w") as f:
     for i,thread in enumerate(threads):
         try:
             print "Loading thread num:%d, id=%s...." % (i,thread.thread_id)
